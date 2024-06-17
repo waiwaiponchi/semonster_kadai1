87f6b2b7102357eec0f98d83ec80d92767807d3d
ukyo <e1n21077@oit.ac.jp>
2024-06-17 13:11:01 +0900

    add semonster main class

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
-    Player Player = new Player();
-    Player.drawMonsters();
-    Player.showDeck();
+    System.out.println("SEMONSTER GAME");
+    System.out.println("show a new monster");
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
 }
