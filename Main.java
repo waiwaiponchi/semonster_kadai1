6db91fad7f5482aee0711ffd83b32a88c17ddce3
ukyo <e1n21077@oit.ac.jp>
2024-06-20 11:06:33 +0900

    初期のPlayer.javaに対するcommit

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("問題4でのjavaプログラム");
-    System.out.println("SEMONSTER GAME");
-    System.out.println("show a new monster");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    System.out.println("SEMonster!");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
